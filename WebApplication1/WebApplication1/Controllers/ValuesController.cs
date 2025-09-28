444dbb2978c530a7cdce819672820f71cf0fbfb7
LSS <LSS@DESKTOP-PK7R2I8>
2025-09-28 16:50:21 +0800

    控制器編輯

--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -7,5 +7,29 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private static readonly string[] Summaries = new[]
+        {
+            "A", "B", "C", "C", "D", "E", "F", "G", "H", "I"
+        };
+
+        private readonly ILogger<ValuesController> _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetValuesForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
+
+    
--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a7169e7d4accbbc95c8aa9c30e718d26c5cd7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69dec3c8ad1a419eb2fe92727c55c07d40a95658")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
